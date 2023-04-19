b1d7701220adfecd94d15967c3793943461255a3
Akira <85637598+Kensukeken@users.noreply.github.com>
2023-04-19 08:34:49 -0400

    Create example_2.cpp

---
+++ Foor Loop/example_2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+  for (int i = 2; i <= 7; i++) {
+    std::cout << i << "\n";
+  }
+  for (int i = 7; i >= 2; i--) {
+    std::cout << i << "\n";
+  }
+  return 0;
+}
